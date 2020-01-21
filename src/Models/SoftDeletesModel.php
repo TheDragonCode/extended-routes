3186e010a4d6625f5a1278c692826ab7d4b64f17
Andrey Helldar <helldar@ai-rus.com>
2020-01-21 15:14:57 +0300

    SoftDeletesModel made abstract

--- src/Models/SoftDeletesModel.php
+++ src/Models/SoftDeletesModel.php
@@ -5,7 +5,7 @@ namespace Helldar\ExtendedRoutes\Models;
 use Helldar\ExtendedRoutes\Traits\SoftDeletesModel as SoftDeletesModelTrait;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 
-class SoftDeletesModel extends EloquentModel
+abstract class SoftDeletesModel extends EloquentModel
 {
     use SoftDeletesModelTrait;
 }
